f04fc321540eae20b7d70c460cfe11d121e2a5e7
erekwang <erekwang@tencent.com>
2021-06-24 14:19:22 +0800

    feat: 做一个脚手架

---
+++ README.md
@@ -0,0 +1,10 @@
+### 参考链接
+ - main https://juejin.cn/post/6966119324478079007
+ - others：
+  - user token request api https://blog.csdn.net/weixin_43274247/article/details/106577653
+    https://blog.csdn.net/u010467784/article/details/78623173
+  - node 使用 download-git-repo 下载 github 代码 https://blog.csdn.net/qq_42535651/article/details/113954798
+
+### use 
+  - npm link
+  - zr create <name> -f
---
+++ bin/cli.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+const program = require('commander')
+
+program
+  .command('create <app-name>')
+  .description('create a new project')
+  .option('-f, --force', 'overwrite target directory if it exist') // 是否强制创建，当文件夹已经存在
+  .action((name, options) => {
+    // 在 create.js 中执行创建任务
+    require('../lib/create.js')(name, options)
+  })
+program
+   // 配置版本号信息
+  .version(`v${require('../package.json').version}`)
+  .usage('<command> [option]')
+  
+// 解析用户执行命令传入参数
+program.parse(process.argv);
